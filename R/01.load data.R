4b0103d716857b6a941af224d173f297516676d2
chrisleitzinger <christelleleitzinger@gmail.com>
2023-05-08 16:57:30 -0400

    Load data script

---
+++ R/01.load data.R
@@ -0,0 +1,30 @@
+# Import Library
+library(tidyverse)
+# library(data.table)
+library(lubridate)
+
+
+################################################################################# I ### Load data
+path <- fs::path("", "Volumes", "Gillis_Research","Christelle Colin-Leitzinger", "Ovarian CH")
+
+Demographic <- 
+  readxl::read_xlsx(paste0(path, "/raw data/Report for ovarian patients .xlsx"),
+                    sheet = "Demographics") %>% 
+  janitor::clean_names()
+
+ovarian_DNA <- 
+  readxl::read_xlsx(paste0(path, "/raw data/Report for ovarian patients .xlsx"),
+                    sheet = "Biobanking") %>% 
+  janitor::clean_names()
+
+ovarian_info <- 
+  readxl::read_xlsx(paste0(path, "/raw data/Report for ovarian patients .xlsx"),
+                    sheet = "Cancer Characteristics") %>% 
+  janitor::clean_names()
+
+Chemot <- 
+  readxl::read_xlsx(paste0(path, "/raw data/Report for ovarian patients .xlsx"),
+                    sheet = "Treatments") %>% 
+  janitor::clean_names()
+
+
